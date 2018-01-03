08d5fdcb5a3ff49de7f181f433b29ce2418b55c6
udwivedi394 <utkarshdwivedi394@gmail.com>
2018-01-03 23:57:25 +0530

    Implemented a better method, needs more optimization

--- minimumElemRemoval.py
+++ minimumElemRemoval.py
@@ -25,13 +25,27 @@ def minimumRemovals02(arr):
 
 	return min(minimumRemovals02(arr[1:])+1, minimumRemovals02(arr[:-1])+1)
 
+#Nested solution with two loops, if we can figure out to manage min and max in constant time,
+#the solution will have overall complexity of O(n^2)
 def minimumRemovals03(arr):
 	starting_point=0
-	ending_poin=0
-	pass
+	ending_point=0
+	max_len = 0
+	max_arr = None
+	
+	for starting_point in range(len(arr)):
+		for ending_point in range(starting_point+1,len(arr)+1):
+			mini = min(arr[starting_point:ending_point])
+			maxi = max(arr[starting_point:ending_point])
+			if 2*mini > maxi and ending_point-starting_point > max_len:
+				max_arr = arr[starting_point:ending_point]
+				max_len = ending_point-starting_point
 
+	print max_arr
+	return len(arr)-len(max_arr)
 
 arr1 = [4, 5, 100, 9, 10, 11, 12, 15, 200]
 arr2 = [1,2,3]
 print minimumRemovals(arr1)
 print minimumRemovals02(arr1)
+print minimumRemovals03(arr1)
