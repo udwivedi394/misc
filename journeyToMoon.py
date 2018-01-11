d933a52de659c1c53f1cb1dbbe9432aedb59e514
udwivedi394 <utkarshdwivedi394@gmail.com>
2018-01-11 09:41:14 +0530

    Implemented journeyToMoon

--- journeyToMoon.py
+++ journeyToMoon.py
@@ -1,39 +1,52 @@
+def merge_sets(groups,arr_i):
+	mainreserve = set()
+	while arr_i:
+		i = arr_i.pop()
+		temp = groups.pop(i)
+		mainreserve = mainreserve.union(temp)
+	groups.append(mainreserve)
+	return
+
 def journeyToMoon(n,astronaut):
 	p = len(astronaut)
-	mainset = set(astronaut[0])
-	groups = [set(astronaut[0])]
+	mainset = set()
+	groups = []
 
-	for i in range(1,p):
-		pair = astronaut[i]
-		val = 0 if pair[0] in mainset else -1
-		if val==-1:
-			val = 1 if pair[1] in mainset else -1
-		if val==-1:
-			groups.append(set(pair))	
-	
+	for pair in astronaut:
+		flag1 = True if pair[0] in mainset else False
+		flag2 = True if pair[1] in mainset else False
+
 		mainset.add(pair[0])
 		mainset.add(pair[1])
 
-		all_i = []
-		for i in range(len(groups)):
-			seti = groups[i]
-			if pair[val] in seti:
-				seti.add(pair[val^1])
-				all_i.append(i)
+		if flag1 and flag2:
+			arr_i = []
+			for i in range(len(groups)):
+				seti = groups[i]
+				if pair[0] in seti or pair[1] in seti:
+					arr_i.append(i)
+			#print "Called for:",pair
+			merge_sets(groups,arr_i)
 
-		print all_i
+		elif flag1 or flag2:
+			for i in range(len(groups)):
+				if pair[0] in groups[i] or pair[1] in groups[i]:
+					groups[i].add(pair[0])
+					groups[i].add(pair[1])
+		else:
+			groups.append(set(pair))
+
 	for i in range(n):
 		if i not in mainset:
 			mainset.add(i)
-			groups.append(set([i]))
-	final_arr = []
-	
+			groups.append(set([i]))	
 	sumi = 0
+	final_arr = []
 	for grp in groups:
 		m = len(grp)
 		final_arr.append(m)
 		sumi += m
-
+	
 	result = 0
 	
 	for i in final_arr:
@@ -44,11 +57,11 @@ def journeyToMoon(n,astronaut):
 n = 5
 p = 3
 arr = [[0,1],[2,3],[0,4]]
-arr2 = [[0,2]]
 """
+arr2 = [[0,2]]
 n = 10
 p = 7
 arr = [[0,2],[1,8],[1,4],[2,8],[2,6],[3,5],[6,9]]
 
-print journeyToMoon(n,arr)
-#print journeyToMoon(4,arr2)
+#print journeyToMoon(n,arr)
+print journeyToMoon(4,arr2)
